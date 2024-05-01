1ef439eb86bdff9591a4f77edba7d5b2d7471394
rayliu7717 <rayliu2004@gmail.com>
2024-05-01 17:46:57 -0400

    setup

--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
-import express from "express";
-import {routes} from './routes'
+import express from 'express';
+import bodyParser from 'body-parser';
+import { db } from './db';
+import { routes } from './routes';
 
-const app = express()
+const app = express();
+
+app.use(bodyParser.json());
+
 routes.forEach(route => {
-    app[route.method] (route.path,route.handler)
-})
-/*
-app.get('/hello', (req,res) =>{
-    res.send("Hello  world!")
-})
-*/
+    app[route.method](route.path, route.handler);
+});
 
-app.listen(8080, () => {
-    console.log('Server is listening on port 8080')
-})
+const start = async () => {
+    await db.connect('mongodb+srv://gerry:benray1110@cluster0.pctoxth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    app.listen(8080,() => {
+        console.log('Server is listening on port 8080');
+    });
+}
+
+start();
